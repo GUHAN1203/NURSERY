346e8481fd247a39dd59c5fda0c7dd45ed3d468b
guhan1203 <guhanramu1203@gmail.com>
2024-12-19 18:18:55 +0530

    CONTACT US

--- my-app/src/HERO2/Hero2.js
+++ my-app/src/HERO2/Hero2.js
@@ -4,65 +4,57 @@ import "./Hero2.css"
 
 function Hero2(){
     return(
-        <div >
-            <div  id="contact" >
-           <h5 >    CONTACT US  </h5>
-           <br/>
+     
+<div>
 
-        </div>
-        <div id="address" class="d-flex">
-            <div>
-        <h3> JK FARMS & NURSERY</h3>
-           <br/>
-           <h5>SH 49, Sea Cliff Conclave, Akkarai, Panaiyur,
-            <br/> Chennai, Tamil Nadu 600119</h5>
-           <br/>
-           <h3>CALL : <h5>6382011788</h5> </h3>
-           <br/>
-           <h3>E-MAIL : jkfarms@gmail.com</h3>
-        </div>
-
-
-        <div>
-        <h3>HOURS</h3>
-           <br/>
-           <h3>Monday - Friday: 9:00AM–6:30PM
+<div  id="contact" >
+            <h2 >    CONTACT US  </h2>
             <br/>
-             Saturday- Sunday: 9:00AM–7:00PM</h3>
 
+         </div>
+
+        <div class="container">
+    <div class="row  g-lg-5 py-5">
+      <div class="col-md-10 mx-auto text-center col-lg-5">
+        <h1 class="display-4 fw-bold lh-1 text-body-emphasis  mb-3">JK FARMS & NURSERY</h1>
+        <p class="col-lg-10 fs-4">
+            <br/>
+            <h5>SH 49, Sea Cliff Conclave, Akkarai, Panaiyur,
+             <br/> Chennai, Tamil Nadu 600119</h5>
+            <br/>
+            <h3>CALL : <a class="text-black text-decoration-none" href="tel:6382011788">6382011788</a> </h3>
+            <br/>
+            <h3>E-MAIL : <a class="text-black text-decoration-none" href="mailto:jkfarms@gmail.com"> jkfarms@gmail.com</a></h3></p>
+            <br/>
+      </div>
+      <br/>
+      <div class="col-md-10 mx-auto text-center col-lg-5">
+      <h1 class="display-4 fw-bold lh-1  text-body-emphasis mb-3">HOURS</h1>
+        <p class="col-lg-10  fs-4"><h3>
+            <br/>
+            Monday - Friday: 9:00AM–6:30PM
+             <br/>
+             <br/>
+              Saturday- Sunday: 9:00AM–7:00PM</h3></p>
+      </div>
+    </div>
+  </div>
+  <div id="seeus">
+            <br/>
+            <h3  class="display- fw-bold"> SEE US IN PERSON !</h3>
+            <br/>
+            <h4>The best time to plant a tree was 20 years ago. The second best time is now.</h4>
+            <br/>
+
+
+           </div> 
+
+    <div>
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.218490043949!2d80.24374567430168!3d12.89366766658366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525b64a08b4917%3A0x8c71c1b8893a7f2c!2sJ.%20K%20Farm%20and%20Nursery!5e0!3m2!1sen!2sin!4v1734421757295!5m2!1sen!2sin" width="100%" height="450" style={{border:"0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
         </div>
-
-        </div>
-
-        <div>
-           <br/>
-           <h3> SEE US IN PERSON !</h3>
-           <br/>
-           <h5>The best time to plant a tree was 20 years ago. The second best time is now.</h5>
-           <br/>
-
-
-                </div> 
-
-            <div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.218490043949!2d80.24374567430168!3d12.89366766658366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525b64a08b4917%3A0x8c71c1b8893a7f2c!2sJ.%20K%20Farm%20and%20Nursery!5e0!3m2!1sen!2sin!4v1734421757295!5m2!1sen!2sin" width="100%" height="450" style={{border:"0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
-            </div>
-            
-
-
-
-
-
-
-
-        </div>
-
-
-
-
-
+  </div>
+   
 
     )
 }
