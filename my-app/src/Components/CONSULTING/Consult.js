afc2f5a18a53119869d74e51aa32f8f8cd8b7899
guhan1203 <guhanramu1203@gmail.com>
2025-01-25 00:30:31 +0530

    consulting

--- my-app/src/Components/CONSULTING/Consult.js
+++ my-app/src/Components/CONSULTING/Consult.js
@@ -25,7 +25,7 @@ function Consult(){
         </div>
         <div className='section2'>
     
-          <p style={{ textAlign:'justify', fontSize:"x-large"}}>
+          <p style={{ textAlign:'justify', fontSize:"1.45rem"}}>
           At JK FARMS & NURSERY, we understand that a well-maintained garden is more than just beautiful – it's an investment in your property's value and your own enjoyment. Our experienced team offers a comprehensive range of services, including lawn care, planting, pruning, and weed control, all performed with a focus on sustainability and customer satisfaction. We utilize eco-friendly practices whenever possible and offer customized solutions to meet your unique needs and budget. Let us take the worry out of garden maintenance and help you create an outdoor space you'll love. Contact us today for a free consultation and discover the difference professional garden care can make.
           </p>
         </div>
@@ -42,7 +42,7 @@ function Consult(){
         </div>
         <div className='section2'>
       
-          <p style={{ textAlign:'justify',fontSize:"x-large"}}>
+          <p style={{ textAlign:'justify',fontSize:"1.45rem"}}>
           Transform your outdoor spaces into stunning landscapes with our expert design and installation services. Whether you're a nursery seeking to showcase your plants in the most captivating way or a farm looking to create a visually appealing and functional environment, our team can help. We specialize in creating unique and sustainable landscapes that enhance the beauty and value of your property. From selecting and planting the perfect trees and shrubs to designing intricate water features and creating inviting outdoor living areas, we provide a full range of services to bring your vision to life. Contact us today for a free consultation and let us help you create a truly exceptional landscape
           </p>
         </div>
@@ -60,7 +60,7 @@ function Consult(){
         </div>
         <div className='section2'>
         
-          <p style={{ textAlign:'justify',fontSize:"x-large"}}>
+          <p style={{ textAlign:'justify',fontSize:"1.45rem"}}>
          
           Experience the timeless beauty and enduring strength of stone with our exquisite stone landscaping services. We transform outdoor spaces into stunning works of art using a diverse range of natural stones, including flagstone, slate, granite, and more. Our skilled craftsmen meticulously design and install pathways, patios, retaining walls, and other features that add both aesthetic appeal and functionality to your property. We prioritize quality materials, expert craftsmanship, and a commitment to creating unique and lasting landscapes that enhance the beauty and value of your home or business. Contact us today for a free consultation and let us bring the elegance of stone to your outdoor living spaces
           </p>
@@ -78,7 +78,7 @@ function Consult(){
         </div>
         <div className='section2'>
         
-          <p style={{ textAlign:'justify',fontSize:"x-large"}}>
+          <p style={{ textAlign:'justify',fontSize:"1.45rem"}}>
           Bring your vision for a thriving garden to life with our expert gardening services. Whether you dream of a vibrant flower bed, a productive vegetable patch, or a serene oasis for relaxation, our skilled gardeners can help. We offer a comprehensive range of services, including planting, pruning, weeding, fertilizing, and ongoing maintenance to ensure your garden thrives throughout the year. We use eco-friendly practices whenever possible and prioritize creating healthy and beautiful gardens that enhance the beauty and enjoyment of your outdoor space. Contact us today for a free consultation and let us transform your gardening dreams into reality.
           </p>
         </div>
--- my-app/src/Components/Hero3/Hero3.js
+++ my-app/src/Components/Hero3/Hero3.js
@@ -15,6 +15,7 @@ import { EffectCoverflow, Pagination, Autoplay } from 'swiper/modules';
 export default function Hero3() {
   return (
     <>
+    
     <div className='home-products'>
         <h1>PRODUCTS</h1>
     </div>
@@ -22,7 +23,7 @@ export default function Hero3() {
         effect={"coverflow"}
         grabCursor={true}
         autoplay={{
-          delay: 3000, // Delay between slides (3 seconds)
+          delay:1500, // Delay between slides (3 seconds)
           disableOnInteraction: false, // Keeps autoplay after interaction          
           pauseOnMouseEnter: true, // Pause autoplay when hovering over the swiper
         }}
@@ -47,44 +48,44 @@ export default function Hero3() {
             <a  href='product'>
             <img src="https://images.unsplash.com/photo-1538998073820-4dfa76300194?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className='card-img' alt='img' />
             </a>
-            <a href='product' className='card-btn'> LandScaping</a>
+            <a href='product' className='card-btn'> FLOWERS</a>
             </div>
         </SwiperSlide>
         <SwiperSlide>
         <div className='card-container'>
         <img src="https://plus.unsplash.com/premium_photo-1676475964992-6404b8db0b53?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className='card-img'/>
-            <a href='consulting' className='card-btn'> LandScaping</a>
+            <a href='consulting' className='card-btn'> INDOOR PLANTS</a>
             </div>
           
         </SwiperSlide>
         <SwiperSlide>
         <div className='card-container'>
         <img src="https://plus.unsplash.com/premium_photo-1676475964992-6404b8db0b53?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className='card-img'/>
-            <a href='consulting' className='card-btn'> LandScaping</a>
+            <a href='consulting' className='card-btn'> LANDSCAPING</a>
             </div>
         </SwiperSlide>
         <SwiperSlide>
         <div className='card-container'>
         <img src="https://plus.unsplash.com/premium_photo-1676475964992-6404b8db0b53?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className='card-img'/>
-            <a href='consulting' className='card-btn'> LandScaping</a>
+            <a href='consulting' className='card-btn'> TREES</a>
             </div>
         </SwiperSlide>
         <SwiperSlide>
         <div className='card-container'>
         <img src="https://plus.unsplash.com/premium_photo-1674986175088-2d7dda41f7f8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZsb3dlcnN8ZW58MHx8MHx8fDA%3D" className='card-img'/>
-            <a href='consulting' className='card-btn'> LandScaping</a>
+            <a href='consulting' className='card-btn'> OUTDOOR PLANTS</a>
             </div>
         </SwiperSlide>
         <SwiperSlide>
         <div className='card-container'>
         <img src="https://images.unsplash.com/photo-1538998073820-4dfa76300194?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className='card-img'/>
-            <a href='consulting' className='card-btn'> LandScaping</a>
+            <a href='consulting' className='card-btn'> FRUIT PLANTS</a>
             </div>
         </SwiperSlide>
         <SwiperSlide>
         <div className='card-container'>
         <img src="https://images.unsplash.com/photo-1495975295218-a59edae1540a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTExfHxmbG93ZXJzfGVufDB8MXwwfHx8MA%3D%3D" className='card-img'/>
-            <a href='consulting' className='card-btn'> LandScaping</a>
+            <a href='consulting' className='card-btn'> FERTILIZER </a>
             </div>
         </SwiperSlide>
       </Swiper>
