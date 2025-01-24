8be572d3a6f08f7886f469602155247e842b5e59
Bhavan663 <venkateshbhavankalyan@gmail.com>
2025-01-25 00:08:40 +0530

    25-01-25 12.08am

--- my-app/src/Components/About/About.js
+++ my-app/src/Components/About/About.js
@@ -6,13 +6,11 @@ function About(){
     <div>
       <div class="cover">
         <h1 class="heading">ABOUT US</h1>
-        <img src="/assets/images/CONSULTING/cover.jpg"></img>
+        <img src="/assets/images/HERO/carousel1.jpg"></img>
     </div>
-        <div style={{ color: 'black', backgroundColor: 'blue', textAlign: 'center', padding: '50px 80px', textAlign: 'justify' }}>
-          <h3 style={{ textAlign: 'center' }}>FOUNDERS</h3>
+        <div  className='founder-heading'>
+          <h1 style={{ textAlign: 'center' }}>FOUNDERS</h1>
         </div>
-        <br/>
-
         <div className='founder1'>
           <div className='column1'>
            <img src='https://images.unsplash.com/photo-1737145907111-c3372a608a45?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0fEpwZzZLaWRsLUhrfHxlbnwwfHx8fHw%3D'></img>
